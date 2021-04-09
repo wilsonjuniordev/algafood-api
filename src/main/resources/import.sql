acb7fbd9e215d31da95feb2b412daa92bf9e75a8
wilsonjuniordev <wilsonwj0101@gmail.com>
2021-04-09 20:44:27 -0300

    Ajusta properties e imports

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,24 +1,24 @@
-insert into cozinha (id, nome) values (1, 'Tailandesa');
-insert into cozinha (id, nome) values (2, 'Indiana');
+insert into cozinha (nome) values ('Tailandesa');
+insert into cozinha (nome) values ('Indiana');
 
 insert into restaurante (nome, taxa_frete, cozinha_id) values ('Thai Gourmet' , 10.0, 1);
 insert into restaurante (nome, taxa_frete, cozinha_id) values ('Thai Delivery' , 9.50, 1);
 insert into restaurante (nome, taxa_frete, cozinha_id) values ('Tuk Tuk Comida Indiana' , 15.0, 2);
 
-insert into forma_pagamento (id, descricao) values (1, 'Cartão de crédito');
-insert into forma_pagamento (id, descricao) values (2, 'Cartão de débito');
-insert into forma_pagamento (id, descricao) values (3, 'Dinheiro');
+insert into forma_pagamento (descricao) values ('Cartão de crédito');
+insert into forma_pagamento (descricao) values ('Cartão de débito');
+insert into forma_pagamento (descricao) values ('Dinheiro');
 
-insert into permissao (id, nome, descricao) values (1, 'CONSULTAR_COZINHAS', 'Permite consultar cozinhas');
-insert into permissao (id, nome, descricao) values (2, 'EDITAR_COZINHAS', 'Permite editar cozinhas');
+insert into permissao (nome, descricao) values ('CONSULTAR_COZINHAS', 'Permite consultar cozinhas');
+insert into permissao (nome, descricao) values ('EDITAR_COZINHAS', 'Permite editar cozinhas');
 
-insert into estado (id, nome) values (1, 'Rio de Janeiro');
-insert into estado (id, nome) values (2, 'São Paulo');
-insert into estado (id, nome) values (3, 'Minas Gerais');
+insert into estado (nome) values ('Rio de Janeiro');
+insert into estado (nome) values ('São Paulo');
+insert into estado (nome) values ('Minas Gerais');
 
-insert into cidade (id, nome, estado_id) values (5, 'Rio de Janeiro', 1);
-insert into cidade (id, nome, estado_id) values (6, 'Niterói', 1);
-insert into cidade (id, nome, estado_id) values (3, 'São Paulo', 2);
-insert into cidade (id, nome, estado_id) values (4, 'Campinas', 2);
-insert into cidade (id, nome, estado_id) values (1, 'Uberlândia', 3);
-insert into cidade (id, nome, estado_id) values (2, 'Belo Horizonte', 3);
+insert into cidade (nome, estado_id) values ('Rio de Janeiro', 1);
+insert into cidade (nome, estado_id) values ('Niterói', 1);
+insert into cidade (nome, estado_id) values ('São Paulo', 2);
+insert into cidade (nome, estado_id) values ('Campinas', 2);
+insert into cidade (nome, estado_id) values ('Uberlândia', 3);
+insert into cidade (nome, estado_id) values ('Belo Horizonte', 3);
