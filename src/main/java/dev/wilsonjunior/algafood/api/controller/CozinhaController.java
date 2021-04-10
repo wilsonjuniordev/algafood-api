070417d1bb3110dbc75717f13689b0f6e41d3934
wilsonjuniordev <wilsonwj0101@gmail.com>
2021-04-10 12:51:58 -0300

    Implementa servico de cozinha

--- src/main/java/dev/wilsonjunior/algafood/api/controller/CozinhaController.java
+++ src/main/java/dev/wilsonjunior/algafood/api/controller/CozinhaController.java
@@ -2,21 +2,28 @@ package dev.wilsonjunior.algafood.api.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.wilsonjunior.algafood.api.model.CozinhasXmlWrapper;
+import dev.wilsonjunior.algafood.domain.exception.EntidadeEmUsoException;
+import dev.wilsonjunior.algafood.domain.exception.EntidadeNaoEncontradaException;
 import dev.wilsonjunior.algafood.domain.model.Cozinha;
 import dev.wilsonjunior.algafood.domain.repository.CozinhaRepository;
+import dev.wilsonjunior.algafood.domain.service.CadastroCozinhaService;
 
 @RestController
 @RequestMapping("/cozinhas")
@@ -24,6 +31,9 @@ public class CozinhaController {
 
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
+
+	@Autowired
+	private CadastroCozinhaService cadastroCozinha;
 
 	@GetMapping
 	public List<Cozinha> listar() {
@@ -49,7 +59,38 @@ public class CozinhaController {
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public Cozinha adicionar(@RequestBody Cozinha cozinha) {
-		return cozinhaRepository.salvar(cozinha);
+		return cadastroCozinha.salvar(cozinha);
+	}
+
+	@PutMapping("/{cozinhaId}")
+	public ResponseEntity<Cozinha> atualizar(@PathVariable Long cozinhaId, @RequestBody Cozinha cozinha) {
+		Cozinha cozinhaAtual = cozinhaRepository.buscar(cozinhaId);
+
+		if (cozinhaAtual != null) {
+			BeanUtils.copyProperties(cozinha, cozinhaAtual, "id");
+
+			cozinhaAtual = cozinhaRepository.salvar(cozinhaAtual);
+			return ResponseEntity.ok(cozinhaAtual);
+		}
+
+		return ResponseEntity.notFound().build();
+	}
+
+	@DeleteMapping("/{cozinhaId}")
+	public ResponseEntity<Cozinha> remover(@PathVariable Long cozinhaId) {
+
+		try {
+			cadastroCozinha.excluir(cozinhaId);
+
+			return ResponseEntity.noContent().build();
+
+		} catch (EntidadeNaoEncontradaException e) {
+			return ResponseEntity.notFound().build();
+
+		} catch (EntidadeEmUsoException e) {
+			return ResponseEntity.status(HttpStatus.CONFLICT).build();
+		}
+
 	}
 
 }
---
+++ src/main/java/dev/wilsonjunior/algafood/domain/service/CadastroCozinhaService.java
@@ -0,0 +1,35 @@
+package dev.wilsonjunior.algafood.domain.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import dev.wilsonjunior.algafood.domain.exception.EntidadeEmUsoException;
+import dev.wilsonjunior.algafood.domain.exception.EntidadeNaoEncontradaException;
+import dev.wilsonjunior.algafood.domain.model.Cozinha;
+import dev.wilsonjunior.algafood.domain.repository.CozinhaRepository;
+
+@Service
+public class CadastroCozinhaService {
+
+	public static final String MSG_COZINHA_NAO_EXISTE = "Não existe um cadastro de cozinha com código %d";
+	public static final String MSG_COZINHA_ESTA_EM_USO = "Cozinha de código %d não pode ser removida, pois está em uso";
+
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
+
+	public Cozinha salvar(Cozinha cozinha) {
+		return cozinhaRepository.salvar(cozinha);
+	}
+
+	public void excluir(Long cozinhaId) {
+		try {
+			cozinhaRepository.remover(cozinhaId);
+		} catch (EmptyResultDataAccessException e) {
+			throw new EntidadeNaoEncontradaException(String.format(MSG_COZINHA_NAO_EXISTE, cozinhaId));
+		} catch (DataIntegrityViolationException e) {
+			throw new EntidadeEmUsoException(String.format(MSG_COZINHA_ESTA_EM_USO, cozinhaId));
+		}
+	}
+}
