fbb812dbd8690c4b41f0f4fe3c88155d282787e7
wilsonjuniordev <wilsonwj0101@gmail.com>
2021-04-11 17:42:50 -0300

    Implementa a inclusao de recursos de restaurante

--- src/main/java/dev/wilsonjunior/algafood/domain/service/CadastroRestauranteService.java
+++ src/main/java/dev/wilsonjunior/algafood/domain/service/CadastroRestauranteService.java
@@ -1,9 +1,15 @@
 package dev.wilsonjunior.algafood.domain.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import dev.wilsonjunior.algafood.domain.exception.EntidadeNaoEncontradaException;
+import dev.wilsonjunior.algafood.domain.model.Cozinha;
+import dev.wilsonjunior.algafood.domain.model.Restaurante;
+import dev.wilsonjunior.algafood.domain.repository.CozinhaRepository;
 import dev.wilsonjunior.algafood.domain.repository.RestauranteRepository;
 
+@Service
 public class CadastroRestauranteService {
 
 	public static final String MSG_COZINHA_NAO_EXISTE = "Não existe um cadastro de cozinha com código %d";
@@ -12,5 +18,19 @@ public class CadastroRestauranteService {
 	@Autowired
 	RestauranteRepository restauranteRepository;
 	
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
+	
+	public Restaurante salvar(Restaurante restaurante) {
+		Long cozinhaId = restaurante.getCozinha().getId();
+		Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+		
+		if (cozinha == null) {
+			throw new EntidadeNaoEncontradaException(String.format(MSG_COZINHA_NAO_EXISTE, cozinhaId));
+		}
+		restaurante.setCozinha(cozinha);
+		
+		return restauranteRepository.salvar(restaurante);
+	}
 	
 }
