7c41e011459ca56b76eb8e81d7d32bd4efa0e2ae
wilsonjuniordev <wilsonwj0101@gmail.com>
2021-05-11 07:05:31 -0300

    Customizes the response with ResponseEntityExceptionHandler

--- src/main/java/dev/wilsonjunior/algafood/domain/exception/EntidadeNaoEncontradaException.java
+++ src/main/java/dev/wilsonjunior/algafood/domain/exception/EntidadeNaoEncontradaException.java
@@ -1,8 +1,5 @@
 package dev.wilsonjunior.algafood.domain.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 public abstract class EntidadeNaoEncontradaException extends NegocioException {
 
 	private static final long serialVersionUID = 1L;
