b51237c47667c234cc4dcd610272b2467b9ecdca
wilsonjuniordev <wilsonwj0101@gmail.com>
2021-05-21 07:58:23 -0300

    creates integration test

---
+++ src/test/java/dev/wilsonjunior/algafood/CadastroCozinhaIntegrationTests.java
@@ -0,0 +1,54 @@
+package dev.wilsonjunior.algafood;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import javax.validation.ConstraintViolationException;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import dev.wilsonjunior.algafood.domain.exception.CozinhaNaoEncontradaException;
+import dev.wilsonjunior.algafood.domain.exception.EntidadeEmUsoException;
+import dev.wilsonjunior.algafood.domain.model.Cozinha;
+import dev.wilsonjunior.algafood.domain.service.CadastroCozinhaService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class CadastroCozinhaIntegrationTests {
+
+	@Autowired
+	private CadastroCozinhaService cadastroCozinha;
+
+	@Test
+	public void deveAtribuirId_QuandoCadastrarCozinhaComDadosCorretos() {
+		Cozinha novaCozinha = new Cozinha();
+		novaCozinha.setNome("Chinesa");
+
+		novaCozinha = cadastroCozinha.salvar(novaCozinha);
+
+		assertThat(novaCozinha).isNotNull();
+		assertThat(novaCozinha.getId()).isNotNull();
+	}
+
+	@Test(expected = ConstraintViolationException.class)
+	public void deveFalhar_QuandoCadastrarCozinhaSemNome() {
+		Cozinha novaCozinha = new Cozinha();
+		novaCozinha.setNome(null);
+
+		novaCozinha = cadastroCozinha.salvar(novaCozinha);
+	}
+	
+	@Test(expected = EntidadeEmUsoException.class)
+	public void deveFalhar_QuandoExcluirCozinhaEmUso() {
+		cadastroCozinha.excluir(1L);
+	}
+
+	@Test(expected = CozinhaNaoEncontradaException.class)
+	public void deveFalhar_QuandoExcluirCozinhaInexistente() {
+		cadastroCozinha.excluir(100L);
+	}
+	
+}
