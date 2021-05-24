eba36b8f70980fea929f369e025cb05c3488e629
wilsonjuniordev <wilsonwj0101@gmail.com>
2021-05-24 09:03:10 -0300

    Validates the body of the http response

--- src/test/java/dev/wilsonjunior/algafood/CadastroCozinhaIntegrationTest.java
+++ src/test/java/dev/wilsonjunior/algafood/CadastroCozinhaIntegrationTest.java
@@ -1,25 +1,35 @@
 package dev.wilsonjunior.algafood;
 
+import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import javax.validation.ConstraintViolationException;
 
-import org.junit.jupiter.api.*;
 //import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import dev.wilsonjunior.algafood.domain.exception.CozinhaNaoEncontradaException;
 import dev.wilsonjunior.algafood.domain.exception.EntidadeEmUsoException;
 import dev.wilsonjunior.algafood.domain.model.Cozinha;
 import dev.wilsonjunior.algafood.domain.service.CadastroCozinhaService;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CadastroCozinhaIntegrationTest {
+	
+	@LocalServerPort
+	private int port;
 
 	@Autowired
 	private CadastroCozinhaService cadastroCozinha;
@@ -56,6 +66,36 @@ class CadastroCozinhaIntegrationTest {
 		assertThrows(CozinhaNaoEncontradaException.class, () -> {
 			cadastroCozinha.excluir(100L);
 		});
+	}
+	
+
+	@Test
+	public void deveRetornarStatus200_QuandoConsultarCozinhas() {
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		
+		given()
+			.basePath("/cozinhas")
+			.port(port)
+			.accept(ContentType.JSON)
+		.when()
+			.get()
+		.then()
+			.statusCode(HttpStatus.OK.value());
+	}
+	
+	@Test
+	public void deveConter4Cozinhas_QuandoConsultarCozinhas() {
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		
+		given()
+			.basePath("/cozinhas")
+			.port(port)
+			.accept(ContentType.JSON)
+		.when()
+			.get()
+		.then()
+			.body("", hasSize(5))
+			.body("nome", hasItems("Indiana", "Tailandesa"));
 	}
 
 }
